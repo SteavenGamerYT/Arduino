70163eaac1fb6b211609cdb414a04724e092d9f5
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-09-22 05:14:36 +0300

    Update sketch_sep15a.ino

--- sketch_sep15a/sketch_sep15a.ino
+++ sketch_sep15a/sketch_sep15a.ino
@@ -13,6 +13,35 @@ void setup(){
 }
 
 void loop(){
+        delay(1000);
+        sevseg.setNumber(9);
+        sevseg.refreshDisplay();
+        delay(1000);
+        sevseg.setNumber(8);
+        sevseg.refreshDisplay();
+        delay(1000);
+        sevseg.setNumber(7);
+        sevseg.refreshDisplay();
+        delay(1000);
+        sevseg.setNumber(6);
+        sevseg.refreshDisplay();
+        delay(1000);
+        sevseg.setNumber(5);
+        sevseg.refreshDisplay();
+        delay(1000);
         sevseg.setNumber(4);
-        sevseg.refreshDisplay();        
+        sevseg.refreshDisplay();
+        delay(1000);
+        sevseg.setNumber(3);
+        sevseg.refreshDisplay();
+        delay(1000);
+        sevseg.setNumber(2);
+        sevseg.refreshDisplay();
+        delay(1000);
+        sevseg.setNumber(1);
+        sevseg.refreshDisplay();
+        delay(1000);
+        sevseg.setNumber(0);
+        sevseg.refreshDisplay();
+        delay(1000);
 }
